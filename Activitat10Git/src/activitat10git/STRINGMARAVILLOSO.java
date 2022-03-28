c84e49a3d356cff32b79b856e48b5fda27d12448
tdiasguev <34676@LAPTOP-HMEPQPU1>
2022-03-28 15:41:11 +0200

    version2

--- Activitat10Git/src/activitat10git/STRINGMARAVILLOSO.java
+++ Activitat10Git/src/activitat10git/STRINGMARAVILLOSO.java
@@ -1,63 +1,92 @@
-
 package activitat10git;
-public class STRINGMARAVILLOSO{
 
-    public static void main(String[] args){
-        
-        
+public class STRINGMARAVILLOSO {
+
+    public static void imprimirEspacios(int n) {
+        for (int i = 0; i < n; i++) System.out.print(" ");
+    }
+    
+    public static void imprimirParteString(String s, int ini, int fin) {
+        for (int i = ini; i < fin; i++) System.out.print(s.charAt(i));
+    }
+
+    public static void main(String[] args) {
+
         String M = "MARAVILLOSO";
-        
+
         System.out.println("LA versió que tenim a github ha estat actualitzada i és mes actual que la que tinc en local");
- 
-        //Imprime los primeros dos triangulos de izq a derecha de la primera parte (parte superios)
-        for (int i =0; i<M.length();i++){
-            //Imprime espacios de la primera parte para el primer triangulo 
-            for (int j=0; j < i  ;++j){
+
+        //Imprime los primeros dos triangulos de izq a derecha de la primera parte (parte superios)       
+        for (int i = 0; i < M.length(); i++) {
+            
+            //Imprime espacios de la primera parte para el primer triangulo
+            imprimirEspacios(i);
+            /*
+            for (int j = 0; j < i; ++j) {
                 System.out.print(" ");
             }
-            //Imprime el primer triangulo de la primer parte 
-            for (int j=i; j < M.length() ;++j){
+            */
+            
+            //Imprime el primer triangulo de la primer parte
+            imprimirParteString(M, i, M.length());
+            /*
+            for (int j = i; j < M.length(); ++j) {
                 System.out.print(M.charAt(j));
             }
+            */
+            
             //Imprime espacios entre el primero y segundo triangulo de la primera parte
-            for (int j=i; j < M.length() ;++j){
+            imprimirEspacios(M.length() - i);
+            /*
+            for (int j = i; j < M.length(); ++j) {
                 System.out.print(" ");
             }
+            */
+            
             //Imprime el segundo triangulo de la primera parte
-            for (int j= M.length() - i - 1; j < M.length(); ++j){
+            imprimirParteString(M, M.length() - i - 1, M.length());
+            /*
+            for (int j = M.length() - i - 1; j < M.length(); ++j) {
                 System.out.print(M.charAt(j));
             }
+            */
+            
             System.out.println();
         }
+        
         //Imprime los segundos dos triangulos de izq a derecha de la segunda parte (parte inferior)
-        for (int i =0; i<M.length();i++){
+        for (int i = 0; i < M.length(); i++) {
+            
             //Imprime primer triangulo de la segunda parte
-            for (int j= i; j<M.length(); ++j){
+            imprimirParteString(M, i, M.length());
+            /*
+            for (int j = i; j < M.length(); ++j) {
                 System.out.print(M.charAt(j));
             }
+            */
+            
             //Imprime espacios entre primer y segundo triangulo
-            for (int j=0; j <= i ;++j){
+            imprimirEspacios(i + 1);
+            /*
+            for (int j = 0; j <= i; ++j) {
                 System.out.print(" ");
             }
-            //Imprime segundo triangulo de la segunda parte  
-            for(int j=0; j < i + 1; ++j){
+            */
+            
+            //Imprime segundo triangulo de la segunda parte
+            imprimirParteString(M, 0, i + 1);
+            /*
+            for (int j = 0; j < i + 1; ++j) {
                 System.out.print(M.charAt(j));
             }
+            */
+            
             System.out.println();
         }
-    } 
-    
-  public static void imprimeText(String s){
-  
-  
-  
-  
-  
-  
-  }  
-    
-    
-    
-    
-    
+    }
+
+    public static void imprimeText(String s) {
+
+    }
+
 }
